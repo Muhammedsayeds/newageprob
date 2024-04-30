2bd6149d615f1e0ce1908cd5ebc551a4fc3364bc
Muhammedsayeds <167220966+Muhammedsayeds@users.noreply.github.com>
2024-04-30 08:45:54 +0530

    Add files via upload

---
+++ Quiz Application with Timer/questions.js
@@ -0,0 +1,132 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Father is aged three times more than his son Ronit. After 8 years, he would be two and a half times of Ronit's age. After further 8 years, how many times would he be of Ronit's age?",
+    answer: "2 times",
+    options: [
+      "2 times",
+      "2 1/2 times",
+      "2 3/4 times",
+      "3 times"
+    ]
+  },
+  {
+    numb: 2,
+    question: "A father said to his son, 'I was as old as you are at the present at the time of your birth.' If the father's age is 38 years now, the son's age five years back was:",
+    answer: "14 years",
+    options: [
+      "14 years",
+      "19 years",
+      "33 years",
+      "38 years"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The sum of ages of 5 children born at the intervals of 3 years each is 50 years. What is the age of the youngest child?",
+    answer: "4 years",
+    options: [
+      "4 years",
+      "8 years",
+      "10 years",
+      "None of these"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Present ages of Sameer and Anand are in the ratio of 5 : 4 respectively. Three years hence, the ratio of their ages will become 11 : 9 respectively. What is Anand's present age in years?",
+    answer: "24",
+    options: [
+      "24",
+      "27",
+      "40",
+      "Cannot be determined"
+    ]
+  },
+  {
+    numb: 5,
+    question: "A is two years older than B who is twice as old as C. If the total of the ages of A, B and C be 27, then how old is B?",
+    answer: "11",
+    options: [
+      "7",
+      "8",
+      "9",
+      "10"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Sachin is younger than Rahul by 7 years. If their ages are in the respective ratio of 7 : 9, how old is Sachin?",
+    answer: "24.5",
+    options: [
+      "16 years",
+      "18 years",
+      "28 years",
+      "24.5 years"
+    ]
+  },
+  {
+    numb: 7,
+    question: "A man is 24 years older than his son. In two years, his age will be twice the age of his son. The present age of his son is:",
+    answer: "22 years",
+    options: [
+      "14 years",
+      "18 years",
+      "20 years",
+      "22 years"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Six years ago, the ratio of the ages of Kunal and Sagar was 6 : 5. Four years hence, the ratio of their ages will be 11 : 10. What is Sagar's age at present?",
+    answer: "16 years",
+    options: [
+      "16 years",
+      "18 years",
+      "20 years",
+      "Cannot be determined"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The sum of the present ages of a father and his son is 60 years. Six years ago, father's age was five times the age of the son. After 6 years, son's age will be:",
+    answer: "20 years",
+    options: [
+      "12 years",
+      "14 years",
+      "18 years",
+      "20 years"
+    ]
+  },
+  {
+    numb: 10,
+    question: "At present, the ratio between the ages of Arun and Deepak is 4 : 3. After 6 years, Arun's age will be 26 years. What is the age of Deepak at present?",
+    answer: "15",
+    options: [
+      "12 years",
+      "15 years",
+      "19 and half",
+      "21 years"
+    ]
+  }
+  
+        
+      
+      
+    
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
